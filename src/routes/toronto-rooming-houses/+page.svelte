b2a40f792fdf68189718cd169f42d410812a41b9
q-schen <qsc@live.ca>
2024-04-22 16:39:21 -0400

    update map component and page

--- src/routes/toronto-rooming-houses/+page.svelte
+++ src/routes/toronto-rooming-houses/+page.svelte
@@ -33,15 +33,15 @@
 
 		<div class="title">
 			<h1>
-				Title!
+				Zoning Changes to Toronto's Multi-Tenant Houses
 			</h1>
 			<h2>
-				SubTitle!
+				How changes in the zoning for Toronto's multi-tenant houses can encourage density
 			</h2>
 			<h2>
 				
 			</h2>
-			<p>Authors</p>
+			<p>Qi-Sheng Chen</p>
 			<p>April 2024</p>
 		</div>
 
@@ -55,29 +55,92 @@
 	<div class="text">
 
 		<p>
-			Hello I am some text! 
+			On March 31, 2024, a new regulatory framework for Multi-Tenant Houses, or Rooming Houses, came into effect.
+			This new regulatory framework was adopted by the Toronto City Council on December 14, 2022, and the Zoning
+			Bylaw was amended to permit multi-tenant houses across Toronto. As part of this new framework, all operators
+			of multi-tenant houses are now required to obtain a license and must meet consistent standards as described
+			in the new Multi-Tenant Houses Licensing Bylaw, which also introduces regular oversight and effective enforcement.
+			You can read more about the new regulatory framework
+			<a href="https://www.toronto.ca/community-people/housing-shelter/multi-tenant-rooming-houses/new-framework-for-multi-tenant-rooming-houses/">here</a>. 
 		</p>
 
-
 		<p>
-			Hello I am some more text! 
+			Before the implementation of this new framework, multi-tenant houses were only permitted in the former cities of
+			Toronto, York, and Etobicoke (as lodging houses). Multi-tenant and lodging houses in the former City of Toronto
+			and Etobicoke were required to be licensed, while York did not require such licencing. The former cities of East
+			York, North York, and Scarborough did not permit multi-tenant houses at all. 
 		</p>
 		
-	</div>
-
-
-
-	<Map/>
-
-	<div class="text">
 		<p>
-			More text!
+			A multi-tenant house or rooming house as per the previous (2022) Zoning Bylaw explained that it was a “building
+			in which living accommodation is provided for more than [two (C areas) or 3 (A & B areas)] persons in separate
+			rooms, each of which may have food preparation facilities or sanitary facilities, but not both. A group home,
+			residential care home, nursing home, retirement home, seniors community house, religious residence, student residence,
+			tourist home, or hotel is not a rooming house.” 
+		</p>
+
+		<p>
+			Below is a map of Toronto of rooming house following the 2022 zoning, indicating where multi-tenant houses were
+			permitted by their maximum number of rooms used for living accommodation. As the Zoning Bylaw indicates that each
+			person must have their own separate room for living accommodation, we assume that areas A and C have a maximum of
+			10 rooms. 
 		</p>
 	</div>
+
+
+
+	<Map layer="old"/>
+
+
+
+
+
 	<div class="text">
-		<h3>Data & Methods</h3>
 		<p>
-			Describe the data sources, etc. here :)
+			Then we have a map updated for the new regulatory framework, also indicating zoning areas by the maximum number
+			of rooms permitted in a multi-tenant house. In the new framework, the former cities of Etobicoke and North York
+			and Scarborough are permitted rooming houses up to six rooms. The former cities of Toronto, East York, and York
+			are permitted 6, 12, or 25 rooms depending on the zoning code and other variables. 
+		</p>
+	</div>
+
+	<Map layer="new"/>
+
+	<div class="text">
+		<p>
+			By comparing these two maps, we can see there have been some significant changes in the areas that permit multi-tenant
+			houses and the maximum number of rooms for the multi-tenant houses. 
+		</p>
+		<p>
+			The largest change is that multi-tenant houses are now permitted in several zoning areas in the former cities of
+			North York, East York, and Scarborough, all of which previously did not permit rooming houses. The inclusion of
+			the residential areas of these 3 former cities increased the total land permitting rooming houses up by (192.3)
+			km2. Both North York and Scarborough now permit rooming houses with a maximum of 6 rooms. In addition to
+			permitting rooming houses with a max of 6 rooms, East York also has zoning areas with permission for up to
+			12 and 25 rooms. 
+		</p>
+		<p>
+			The former city of Etobicoke has also amended many residentially zoned areas to permit multi-tenant houses, with
+			a net total increase of area permitting multi-tenant houses with 6 maximum rooms, from (4.3) km<sup>2</sup> to (56.3) km<sup>2</sup>. 
+			However, this net total increase came at the cost of reducing the cap on number of rooms of its already permissible
+			areas from a maximum of 10 rooms to 6 rooms. At this time it is not clear as to which these areas had their maximum
+			number of rooms reduced by 4 rather than increased by 2, to match the new cap intervals of the new regulatory
+			framework. It would not be unprecedented, as in the former city of York, similar changes were seen for the areas
+			previously allowing a maximum of 10 rooms—some of these were reduced to a maximum of 6 rooms, while others increased
+			to 12 rooms. Other small changes in York were all increases, with residential areas previously not permitting
+			multi-tenant houses now allowing up to 6, 12, and 25 rooms. 
+		</p>
+		<p>
+			For the former city of Toronto, there were very few changes, as most of the permissions were carried over in the new
+			framework. Some areas in former Toronto previously not permitting rooming houses or rooming houses of a maximum of
+			12 rooms now allow up to 25 rooms. 
+		</p>
+		<p>
+			Overall, there was an increase of total area zoned to permit multi-tenant houses, from (46.7) km<sup>2</sup> to (323.1) km<sup>2</sup>, or
+			by almost 600% (591.8%). The area permitting up to a maximum of 6 rooms increased by (1072.2) %, max 12 rooms by (30.6) %,
+			and max 25 rooms by (23.9) %. Multi-tenant houses with up to 10 rooms were eliminated entirely, either converted up to
+			12 rooms or down to 6 rooms, most notably in Etobicoke where all max 10 rooms were reduced to max 6 rooms, meaning 100%
+			of the max 10 rooms areas were lost. 
 		</p>
 	</div>
 
--- src/routes/toronto-rooming-houses/lib/MapZoning.svelte
+++ src/routes/toronto-rooming-houses/lib/MapZoning.svelte
@@ -1,22 +1,28 @@
 <script>
-	
-	import { onMount } from 'svelte';
-    import maplibregl from 'maplibre-gl';
-    import * as pmtiles from 'pmtiles';
-	import torontoBoundary from '../../../assets/toronto/toronto-boundary.geo.json';
-	import transitLines from '../../../assets/toronto/transitLines-toronto.geo.json';
-	import transitStops from '../../../assets/toronto/transitStops-toronto.geo.json';
+	import { onMount } from "svelte";
+	import maplibregl from "maplibre-gl";
+	import * as pmtiles from "pmtiles";
+	import torontoBoundary from "../../../assets/toronto/toronto-boundary.geo.json";
+	import transitLines from "../../../assets/toronto/transitLines-toronto.geo.json";
+	import transitStops from "../../../assets/toronto/transitStops-toronto.geo.json";
 	import BaseLayer from "../../../assets/toronto/toronto.json";
 
+	// import rooming house data layers based on map input
+	export let layer;
+		let layername = "old";
+		if (layer === "new") {
+			layername = "new";
+		};
+	
+	import oldOverlay from "../../../assets/toronto/zoning_rooming_house_overlay_old.geo.json";
+	import newOverlay from "../../../assets/toronto/zoning_rooming_house_overlay_Qnew.geo.json";
 
 
 	let map;
 	let PMTILES_URL = "/gentle-density/toronto.pmtiles";
 
 	let protocol = new pmtiles.Protocol();
-	maplibregl.addProtocol('pmtiles', protocol.tile);
-
-	
+	maplibregl.addProtocol("pmtiles", protocol.tile);
 
 	let pageHeight;
 	let pageWidth;
@@ -24,34 +30,33 @@
 	$: if (pageHeight < 650) {
 		mapHeight = pageHeight - 200;
 	} else {
-		mapHeight = 650
+		mapHeight = 650;
 	}
-  
 
 	const maxBounds = [
 		[-80.0, 42.0], // SW coords
-		[-78.6, 49.5] // NE coords
+		[-78.6, 49.5], // NE coords
 	];
 
-	onMount(() => {
+	onMount(async () => {
 		map = new maplibregl.Map({
-			container: "map", 
+			container: "map" + layername,
 			style: {
-					"version": 8,
-					"name": "Empty",
-					"glyphs": "https://schoolofcities.github.io/fonts/fonts/{fontstack}/{range}.pbf",
-					"sources": {},
-					"layers": [
-						{
-							"id": "background",
-							"type": "background",
-							"paint": {
-								"background-color": "rgba(0,0,0,0)"
-							}
-						}
-					]
-				},
-			center: [-79.36,43.71],
+				version: 8,
+				name: "Empty",
+				glyphs: "https://schoolofcities.github.io/fonts/fonts/{fontstack}/{range}.pbf",
+				sources: {},
+				layers: [
+					{
+						id: "background",
+						type: "background",
+						paint: {
+							"background-color": "rgba(0,0,0,0)",
+						},
+					},
+				],
+			},
+			center: [-79.36, 43.71],
 			zoom: 10,
 			maxZoom: 15,
 			minZoom: 8,
@@ -59,10 +64,10 @@
 			maxBounds: maxBounds,
 			boxZoom: false,
 			touchPitch: false,
-			attributionControl: false
+			attributionControl: false,
 		});
-		map.addControl(new maplibregl.NavigationControl(), 'top-left');
-		map.addControl(new maplibregl.ScaleControl(), 'bottom-left');
+		map.addControl(new maplibregl.NavigationControl(), "top-left");
+		map.addControl(new maplibregl.ScaleControl(), "bottom-left");
 
 		// disable map rotation using right click + drag
 		map.dragRotate.disable();
@@ -72,105 +77,169 @@
 
 		let protoLayers = BaseLayer;
 
-
-		map.on('load', function() {
-
-			map.addSource('protomaps', {
+		map.on("load", function () {
+			map.addSource("protomaps", {
 				type: "vector",
 				url: "pmtiles://" + PMTILES_URL,
 			});
 
-			protoLayers.forEach(e => {
+			protoLayers.forEach((e) => {
 				map.addLayer(e);
 			});
 
-			map.addSource('transitLines', {
-				'type': 'geojson',
-				'data': transitLines,
+			map.addSource("transitLines", {
+				type: "geojson",
+				data: transitLines,
 			});
 			map.addLayer({
-				'id': 'transitLines',
-				'type': 'line',
-				'source': 'transitLines',
-				'layout': {},
-				'paint': {
-					'line-color': '#1d4667',
-					'line-width': 2,
-					'line-opacity': 1
-				}
+				id: "transitLines",
+				type: "line",
+				source: "transitLines",
+				layout: {},
+				paint: {
+					"line-color": "#1d4667",
+					"line-width": 2,
+					"line-opacity": 1,
+				},
 			});
 
-			map.addSource('transitStops', {
-				'type': 'geojson',
-				'data': transitStops,
+			map.addSource("transitStops", {
+				type: "geojson",
+				data: transitStops,
 			});
 			map.addLayer({
-				'id': 'transitStops',
-				'type': 'circle',
-				'source': 'transitStops',
-				'layout': {},
-				'paint': {
-					'circle-color': '#1d4667'
-				}
+				id: "transitStops",
+				type: "circle",
+				source: "transitStops",
+				layout: {},
+				paint: {
+					"circle-color": "#1d4667",
+				},
 			});
 			map.addLayer({
-				'id': 'transitStopsWhite',
-				'type': 'circle',
-				'source': 'transitStops',
-				'layout': {},
-				'paint': {
-					'circle-color': '#fff',
-					'circle-radius': 2,
-					'circle-opacity': 0.42
-				}
+				id: "transitStopsWhite",
+				type: "circle",
+				source: "transitStops",
+				layout: {},
+				paint: {
+					"circle-color": "#fff",
+					"circle-radius": 2,
+					"circle-opacity": 0.42,
+				},
 			});
 
-			map.addSource('torontoBoundary', {
-				'type': 'geojson',
-				'data': torontoBoundary
+			map.addSource("torontoBoundary", {
+				type: "geojson",
+				data: torontoBoundary,
 			});
 			map.addLayer({
-				'id': 'torontoBoundary',
-				'type': 'line',
-				'source': 'torontoBoundary',
-				'layout': {},
-				'paint': {
-					'line-color': '#fff',
-					'line-width': 1,
-					'line-opacity': 1
-				}
+				id: "torontoBoundary",
+				type: "line",
+				source: "torontoBoundary",
+				layout: {},
+				paint: {
+					"line-color": "#fff",
+					"line-width": 1,
+					"line-opacity": 1,
+				},
 			});
-			
-			
-			
+
+
+			// load old or new zoning overlay
+			if (layername === "old") {
+				map.addSource("oldOverlay", {
+					type: "geojson",
+					data: oldOverlay,
+				});
+				
+				map.addLayer({
+					id: "oldOverlay",
+					type: "fill",
+					source: "oldOverlay",
+					layout: {},
+					paint: {
+						// categorized and colour by max rooms
+						"fill-color": [
+							"step",
+							["get", "MAX_ROOMS"],
+							"#fff",
+							6,
+							"#feebe2",
+							10,
+							"#fbb4b9",
+							12,
+							"#f768a1",
+							25,
+							"#ae017e",
+						],
+						// categorized by code but colour by max rooms
+						// 'fill-color': [
+						// 	'match',
+						// 	['string', ['get', 'RMG_STRING']],
+						// 	"A1", '#fbb4b9',
+						// 	"B1", '#feebe2',
+						// 	"B2", '#f768a1',
+						// 	"B3", '#ae017e',
+						// 	"C1", '#fbb4b9',
+						// 	'#fff'
+						// ]
+					},
+				});
+
+			}	else if (layername === "new") {
+				map.addSource('newOverlay', {
+					'type': 'geojson',
+					'data': newOverlay,
+				});
+				map.addLayer({
+					'id': 'newOverlay',
+					'type': 'fill',
+					'source': 'newOverlay',
+					'layout': {},
+					'paint': {
+						'fill-color': [
+							'step',
+							['get', 'MAX_ROOMS'], '#fff',
+							6, '#feebe2',
+							10, '#fbb4b9',
+							12, '#f768a1',
+							25, '#ae017e',
+						]
+					}
+				});
+			};
+
+
+
 			if (pageHeight > 700 && pageWidth > 800) {
-				map.zoomTo(10.5)
+				map.zoomTo(10.5);
 			}
 
 
 		});
-
 	});
-
-	
-
 </script>
 
-
-
-<svelte:window bind:innerHeight={pageHeight} bind:innerWidth={pageWidth}/>
+<svelte:window bind:innerHeight={pageHeight} bind:innerWidth={pageWidth} />
 
 <div id="top-bar">
-	<p>Title of the map!</p>
+	<p>Zoning for Multi-Tenant Houses</p>
 </div>
 
-<div id="map" style="height: {mapHeight}px"></div>
+<div id={"map" + layername} class="map" style="height: {mapHeight}px"></div>
 
-
-
+<div style="position: relative">
+	<div id="rooms-legend" class="legend">
+		<h4>Max Rooms</h4>
+		<div><span style="background-color: #feebe2"></span>6</div>
+		<div><span style="background-color: #fbb4b9"></span>10</div>
+		<div><span style="background-color: #f768a1"></span>12</div>
+		<div><span style="background-color: #ae017e"></span>25</div>
+	</div>
+</div>
 
 <style>
-	#map {
+	.map {
 		width: 100%;
 		border-top: 1px solid var(--brandLightBlue);
 		border-bottom: 1px solid var(--brandLightBlue);
@@ -180,7 +249,13 @@
 		width: calc(100% - 36px);
 		background-color: var(--brandDarkBlue);
 		background-size: 13px 13px;
-		background-image: repeating-linear-gradient(-45deg, #eaf5ff05 0, #eaf5ff05 1.3px, var(--brandDarkBlue) 0, var(--brandDarkBlue) 50%);
+		background-image: repeating-linear-gradient(
+			-45deg,
+			#eaf5ff05 0,
+			#eaf5ff05 1.3px,
+			var(--brandDarkBlue) 0,
+			var(--brandDarkBlue) 50%
+		);
 		color: white;
 		font-size: 17px;
 		font-family: UbuntuMonoRegular, monospace;
@@ -197,5 +272,32 @@
 		padding-bottom: 4px;
 		/* font-size: 14px; */
 	}
-	
+
+	.legend {
+        background-color: #fff;
+        border-radius: 3px;
+        bottom: 25px;
+        box-shadow: 0 1px 2px rgba(0, 0, 0, 0.1);
+        font:
+            12px/20px 'Helvetica Neue',
+            Arial,
+            Helvetica,
+            sans-serif;
+        padding: 10px;
+        position: absolute;
+        right: 20px;
+        z-index: 1;
+    }
+
+    .legend h4 {
+        margin: 0 0 10px;
+    }
+
+    .legend div span {
+        /* border-radius: 50%; */
+        display: inline-block;
+        height: 10px;
+        margin-right: 5px;
+        width: 10px;
+    }
 </style>
